aa2c4e6d4da8518236ea05870e6b404d61dea71d
Julio Andres <julio.olivares.a@gmail.com>
2023-06-23 19:45:12 +0800

    adding missing metadata dependency

--- src-tauri/src/configs.rs
+++ src-tauri/src/configs.rs
@@ -47,6 +47,7 @@ pub fn show_in_folder(path: String) {
 
     #[cfg(target_os = "linux")]
     {
+        use std::fs::metadata;
         use std::path::PathBuf;
         if path.contains(",") {
             // see https://gitlab.freedesktop.org/dbus/dbus/-/issues/76
